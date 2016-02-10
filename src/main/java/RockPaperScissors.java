716ba27c891187f0ff61e276318b9f902ee8efeb
Alissa Paluska and Kyle Derrick <kderrick1988@gmail.com>
2016-02-10 10:18:32 -0800

    spec4 pass: computer acurately generates matching random choice

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -2,22 +2,51 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 import spark.ModelAndView;
+import java.util.Random;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class RockPaperScissors {
   public static void main(String[] args) {}
 
-  public static String checkWinner(String personOneChoice, String personTwoChoice) {
+  public static String twoPlayerGame(String personOneChoice, String personTwoChoice) {
 
     String outcome = "";
-    if (personOneChoice == "Rock" && personTwoChoice == "Scissors") {
+    if (personOneChoice == "Rock" && personTwoChoice == "Scissors" ||        personOneChoice == "Scissors" && personTwoChoice == "Paper" || personOneChoice == "Paper" && personTwoChoice == "Rock" ) {
       outcome = "Player 1 Wins";
-    } else if (personOneChoice == "Rock" && personTwoChoice == "Paper") {
+    } else if (personOneChoice == "Rock" && personTwoChoice == "Paper" || personOneChoice == "Scissors" && personTwoChoice == "Rock" || personOneChoice == "Paper" && personTwoChoice == "Scissors") {
       outcome = "Player 2 Wins";
     } else {
       outcome = "Tie Game";
     }
       return outcome;
     }
-}
+
+
+  public static String onePlayerGame() {
+    Random computerRandom = new Random();
+    Integer computerInteger = computerRandom.nextInt(3);
+    String computerChoice = "";
+    String outcome = "";
+
+    if (computerInteger == 0) {
+      computerChoice = "Rock";
+    } else if (computerInteger == 1) {
+      computerChoice = "Scissors";
+    } else {
+      computerChoice = "Paper";
+    }
+
+    return computerChoice;
+
+    // if (player == "Rock" && computerChoice == "Scissors" || player == "Scissors" && computerChoice == "Paper" || player == "Paper" && computerChoice == "Rock" ) {
+    //   outcome = "Player 1 Wins";
+    // } else if (player == "Rock" && computerChoice == "Paper" || player == "Scissors" && computerChoice == "Rock" || player == "Paper" && computerChoice == "Scissors") {
+    //   outcome = "Computer Wins";
+    // } else {
+    //   outcome = "Tie Game";
+    // }
+    //   return outcome;
+    }
+
+  }
--- src/test/java/RockPaperScissorsTest.java
+++ src/test/java/RockPaperScissorsTest.java
@@ -1,24 +1,48 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Random;
 
 public class RockPaperScissorsTest {
 
   @Test
-  public void checkWinner_rockBeatsScissors_player1() {
+  public void twoPlayerGame_rockBeatsScissors_player1() {
   RockPaperScissors testRockPaperScissors = new RockPaperScissors();
   String outcome = "Player 1 Wins";
-  assertEquals(outcome, testRockPaperScissors.checkWinner("Rock", "Scissors"));
+  assertEquals(outcome, testRockPaperScissors.twoPlayerGame("Rock", "Scissors"));
+  assertEquals(outcome, testRockPaperScissors.twoPlayerGame("Scissors", "Paper"));
+  assertEquals(outcome, testRockPaperScissors.twoPlayerGame("Paper", "Rock"));
   }
   @Test
-  public void checkWinner_rockLosePaper_player2() {
+  public void twoPlayerGame_rockLosePaper_player2() {
   RockPaperScissors testRockPaperScissors = new RockPaperScissors();
   String outcome = "Player 2 Wins";
-  assertEquals(outcome, testRockPaperScissors.checkWinner("Rock", "Paper"));
+  assertEquals(outcome, testRockPaperScissors.twoPlayerGame("Rock", "Paper"));
+  assertEquals(outcome, testRockPaperScissors.twoPlayerGame("Scissors", "Rock"));
+  assertEquals(outcome, testRockPaperScissors.twoPlayerGame("Paper", "Scissors"));
   }
   @Test
-  public void checkWinner_rockTieRock_tie() {
+  public void twoPlayerGame_rockTieRock_tie() {
   RockPaperScissors testRockPaperScissors = new RockPaperScissors();
   String outcome = "Tie Game";
-  assertEquals(outcome, testRockPaperScissors.checkWinner("Rock", "Rock"));
+  assertEquals(outcome, testRockPaperScissors.twoPlayerGame("Rock", "Rock"));
   }
+//End of Two Player Game
+//Start One Player Game Against Computer
+  @Test
+  public void onePlayerGame_computerChoiceRandomNumberEqualsChoice_Choice() {
+  RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+  Random computerRandom = new Random();
+  Integer computerInteger = computerRandom.nextInt(3);
+
+  if (computerInteger == 0) {
+    computerChoice = "Rock";
+  } else if (computerInteger == 1) {
+    computerChoice = "Scissors";
+  } else {
+    computerChoice = "Paper";
+  }
+  assertEquals(computerChoice, testRockPaperScissors.onePlayerGame());
+  }
+
+
 }
